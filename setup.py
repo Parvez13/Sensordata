b1950ed96fcdd7542b1c224d8dec890d156a6a7a
Parvez13 <prvzsohail@gmail.com>
2022-12-01 10:56:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ HYPEN_E_DOT = "-e ."
 def get_requirements()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_list:
         requirement_list = requirement_file.readlines()
-    requirement_list = [requirement_name.replace("/n", "") for requirement_name in requirement_list]
+    requirement_list = [requirement_name.replace("/n", " ") for requirement_name in requirement_list]
     
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPEN_E_DOT)
-    requirement_list
+    return requirement_list
 
 setup(
     name="sensor",
