b79043fbd527483b793307dd59407a5000448d85
Parvez13 <prvzsohail@gmail.com>
2022-12-08 20:22:21 +0530

    Update files data_validation

--- sensor/utils.py
+++ sensor/utils.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sensor.config import mongo_client
 from sensor.logger import logging 
 from sensor.exception import SensorException
+import yaml
+import sys, os
 
 def get_collection_as_dataframe(database_name:str, collection_name:str):
     """
@@ -20,6 +22,25 @@ def get_collection_as_dataframe(database_name:str, collection_name:str):
             logging.info(f"Row and columns in df: {df.shape}")
             df = df.drop("_id", axis=1)
         logging.info(f"Row and columns in df: {df.shape}")
+        return df
+    except Exception as e:
+        raise SensorException(e, sys)
+
+
+def write_yaml_file(file_path, data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir, exist_ok=True)
+        with open(file_path, 'w') as file_writer:
+            yaml.dump(data, file_writer)
+    except Exception as e:
+        raise SensorException(e, sys)
+
+def convert_columns_float(df:pd.DataFrame, exclude_columns:list):
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]=df[column].astype('float')
         return df
     except Exception as e:
         raise SensorException(e, sys)
